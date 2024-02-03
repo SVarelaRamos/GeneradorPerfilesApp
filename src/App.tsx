37006dbcb18497acac3daa3f1e438ba0e4214c47
hermosindo <sergiogalego@gmail.com>
2024-02-04 00:31:47 +0100

    Added container class

--- src/App.tsx
+++ src/App.tsx
@@ -101,99 +101,107 @@ function App() {
 
 	return (
 		<>
-			<header>
-				<div className='flex flex-grow flex-row justify-end gap-3'>
-					{avaliableLangs.map((lng: string) => (
-						<button
-							key={lng}
-							style={{
-								fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal'
-							}}
-							className={`justify-start rounded-md border px-4 py-1 text-xs hover:bg-gradient-to-bl hover:shadow-md active:bg-gradient-to-bl ${
-								componentsColors.base
-							} ${
-								i18n.resolvedLanguage === lng
-									? `bg-gradient-to-bl shadow-md ${componentsColors.selected}`
-									: ''
-							}`}
-							type='submit'
-							onClick={async () => await i18n.changeLanguage(lng)}
-						>
-							{lngs[lng as keyof LangsType].nativeName}
-						</button>
-					))}
-				</div>
-				<h1 className='mt-16'>{t('header.title')}</h1>
-			</header>
-			<main
-				className={`mt-8 rounded-xl border p-10 shadow-2xl backdrop-blur-md backdrop-brightness-90 ${colors.border} ${colors.shadow}`}
-			>
-				<section className='flex w-full flex-row flex-wrap justify-between gap-16'>
-					<div className='flex flex-row justify-start'>
-						<form onSubmit={handleSubmit} className='flex flex-row justify-start gap-5'>
+			<header className='w-full'>
+				<div className='container mx-auto '>
+					<div className='flex flex-grow flex-row justify-end gap-3 px-4'>
+						{avaliableLangs.map((lng: string) => (
 							<button
-								className={`justify-start rounded-md border px-6 py-1 hover:bg-gradient-to-bl hover:shadow-md active:bg-gradient-to-bl ${componentsColors.base}`}
+								key={lng}
+								style={{
+									fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal'
+								}}
+								className={`justify-start rounded-md border px-4 py-1 text-xs hover:bg-gradient-to-bl hover:shadow-md active:bg-gradient-to-bl ${
+									componentsColors.base
+								} ${
+									i18n.resolvedLanguage === lng
+										? `bg-gradient-to-bl shadow-md ${componentsColors.selected}`
+										: ''
+								}`}
 								type='submit'
+								onClick={async () => await i18n.changeLanguage(lng)}
 							>
-								{t('button.generate')}
+								{lngs[lng as keyof LangsType].nativeName}
 							</button>
-							<div className='flex flex-row justify-start gap-1'>
-								<span
-									className={`w-12 rounded-md border py-1 text-center ${colors.text} ${colors.simpleBg}`}
-								>
-									{profileNum}
-								</span>
-								<button
-									onClick={handleClickMinus}
-									className={`justify-start rounded-md border px-4 py-1 hover:bg-gradient-to-bl hover:shadow-md active:bg-gradient-to-b ${componentsColors.base}`}
-									type='button'
-								>
-									-
-								</button>
-								<button
-									onClick={handleClickPlus}
-									className={`justify-start rounded-md border px-4 py-1 hover:bg-gradient-to-bl hover:shadow-md active:bg-gradient-to-b ${componentsColors.base}`}
-									type='button'
-								>
-									+
-								</button>
-							</div>
-						</form>
+						))}
 					</div>
-					<div className='flex flex-row flex-wrap justify-start gap-1'>
-						<button onClick={handleClickUpercase} className={classNameUpercaseButton} type='button'>
-							{t('button.uppercase')}
-						</button>
-						<button
-							onClick={handleClickLowercase}
-							className={classNameLowercaseButton}
-							type='button'
-						>
-							{t('button.lowercase')}
-						</button>
-						<button
-							onClick={handleClickStartCase}
-							className={classNameStartcaseButton}
-							type='button'
-						>
-							{t('button.startcase')}
-						</button>
-					</div>
-				</section>
-				{profiles.map((profile, index) => (
-					<Profile key={String(index)} {...formatProfile(profile, stringFormat)} />
-				))}
-				<Tooltip
-					id='copy-field-tooltip'
-					style={{
-						backgroundColor: colors.tooltip.dark.background,
-						fontSize: '0.75rem',
-						lineHeight: '1rem',
-						color: colors.tooltip.dark.text
-					}}
-					anchorSelect='.copy-field-tooltip'
-					content={t('tooltip.clickToCopy')}
-				/>
+					<h1 className='mt-16'>{t('header.title')}</h1>
+				</div>
+			</header>
+			<main className='w-full'>
+				<div
+					className={`container mx-auto mt-8 rounded-xl border p-10 px-4 shadow-2xl backdrop-blur-md backdrop-brightness-90 ${colors.border} ${colors.shadow}`}
+				>
+					<section className='flex w-full flex-row flex-wrap justify-between gap-16'>
+						<div className='flex flex-row justify-start'>
+							<form onSubmit={handleSubmit} className='flex flex-row justify-start gap-5'>
+								<button
+									className={`justify-start rounded-md border px-6 py-1 hover:bg-gradient-to-bl hover:shadow-md active:bg-gradient-to-bl ${componentsColors.base}`}
+									type='submit'
+								>
+									{t('button.generate')}
+								</button>
+								<div className='flex flex-row justify-start gap-1'>
+									<span
+										className={`w-12 rounded-md border py-1 text-center ${colors.text} ${colors.simpleBg}`}
+									>
+										{profileNum}
+									</span>
+									<button
+										onClick={handleClickMinus}
+										className={`justify-start rounded-md border px-4 py-1 hover:bg-gradient-to-bl hover:shadow-md active:bg-gradient-to-b ${componentsColors.base}`}
+										type='button'
+									>
+										-
+									</button>
+									<button
+										onClick={handleClickPlus}
+										className={`justify-start rounded-md border px-4 py-1 hover:bg-gradient-to-bl hover:shadow-md active:bg-gradient-to-b ${componentsColors.base}`}
+										type='button'
+									>
+										+
+									</button>
+								</div>
+							</form>
+						</div>
+						<div className='flex flex-row flex-wrap justify-start gap-1'>
+							<button
+								onClick={handleClickUpercase}
+								className={classNameUpercaseButton}
+								type='button'
+							>
+								{t('button.uppercase')}
+							</button>
+							<button
+								onClick={handleClickLowercase}
+								className={classNameLowercaseButton}
+								type='button'
+							>
+								{t('button.lowercase')}
+							</button>
+							<button
+								onClick={handleClickStartCase}
+								className={classNameStartcaseButton}
+								type='button'
+							>
+								{t('button.startcase')}
+							</button>
+						</div>
+					</section>
+					{profiles.map((profile, index) => (
+						<Profile key={String(index)} {...formatProfile(profile, stringFormat)} />
+					))}
+					<Tooltip
+						id='copy-field-tooltip'
+						style={{
+							backgroundColor: colors.tooltip.dark.background,
+							fontSize: '0.75rem',
+							lineHeight: '1rem',
+							color: colors.tooltip.dark.text
+						}}
+						anchorSelect='.copy-field-tooltip'
+						content={t('tooltip.clickToCopy')}
+					/>
+				</div>
 			</main>
 		</>
 	)
